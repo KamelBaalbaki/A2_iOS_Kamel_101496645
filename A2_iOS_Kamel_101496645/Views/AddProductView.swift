5ccee271e800a1ef46c21964da6ca1604936c4b2
KamelBaalbaki <baalbaki.kamel@gmail.com>
2026-03-30 20:27:53 -0400

    Add product completed

--- A2_iOS_Kamel_101496645/Views/AddProductView.swift
+++ A2_iOS_Kamel_101496645/Views/AddProductView.swift
@@ -5,3 +5,30 @@
 //  Created by Kamel Baalbaki on 2026-03-30.
 //
 
+struct AddProductView: View {
+    @ObservedObject var vm: ProductViewModel
+    
+    @State private var name = ""
+    @State private var desc = ""
+    @State private var price = ""
+    @State private var provider = ""
+    
+    var body: some View {
+        Form {
+            TextField("Name", text: $name)
+            TextField("Description", text: $desc)
+            TextField("Price", text: $price)
+            TextField("Provider", text: $provider)
+            
+            Button("Add Product") {
+                vm.addProduct(
+                    name: name,
+                    desc: desc,
+                    price: Double(price) ?? 0,
+                    provider: provider
+                )
+            }
+        }
+        .navigationTitle("Add Product")
+    }
+}
