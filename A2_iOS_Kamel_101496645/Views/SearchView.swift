0d3f2e3a0f24d8dfacd68062366fb522e9fc87d3
KamelBaalbaki <baalbaki.kamel@gmail.com>
2026-03-30 20:30:45 -0400

    product search view completed

--- A2_iOS_Kamel_101496645/Views/SearchView.swift
+++ A2_iOS_Kamel_101496645/Views/SearchView.swift
@@ -5,3 +5,24 @@
 //  Created by Kamel Baalbaki on 2026-03-30.
 //
 
+struct SearchView: View {
+    @ObservedObject var vm: ProductViewModel
+    @State private var query = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Search...", text: $query)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
+            Button("Search") {
+                vm.search(query: query)
+            }
+            
+            List(vm.products) { product in
+                Text(product.name ?? "")
+            }
+        }
+        .navigationTitle("Search")
+    }
+}
