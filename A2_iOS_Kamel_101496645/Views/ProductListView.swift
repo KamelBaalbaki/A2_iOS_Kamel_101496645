7179caa3efa1e4afddd3aa46708587e8d2db146e
KamelBaalbaki <baalbaki.kamel@gmail.com>
2026-03-30 20:24:53 -0400

    product list completed

--- A2_iOS_Kamel_101496645/Views/ProductListView.swift
+++ A2_iOS_Kamel_101496645/Views/ProductListView.swift
@@ -5,3 +5,22 @@
 //  Created by Kamel Baalbaki on 2026-03-30.
 //
 
+struct ProductListView: View {
+    @ObservedObject var vm: ProductViewModel
+    
+    var body: some View {
+        List {
+            ForEach(vm.products) { product in
+                VStack(alignment: .leading) {
+                    Text(product.name ?? "")
+                        .font(.headline)
+                    
+                    Text(product.desc ?? "")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+        .navigationTitle("All Products")
+    }
+}
